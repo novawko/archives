b66203814c4803cf431dd667df536f8aa0dc21eb
Nova Wolfcreek <novawko@gmail.com>
2024-10-21 19:06:23 -0500

    Update charadex.js

--- styles/charadex.js
+++ styles/charadex.js
@@ -62,7 +62,7 @@ let optionSorter = (options) => {
     let userOptions = options;
     let defaultOptions = {
 
-        sheetID: scrubbedSheetId,
+        sheetID: 1V1l2MQSRTKofZb0Y3JJ8qyak4Iutf8pLhhCybdAIsWw,
         sheetPage: userOptions.sheetPage ? userOptions.sheetPage : "masterlist",
 
         fauxFolderColumn: userOptions.fauxFolderColumn ? keyCreator(userOptions.fauxFolderColumn) : false,
