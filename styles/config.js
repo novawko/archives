3e348b2eca09cf3becfccc9796f63765165cfb02
Nova Wolfcreek <novawko@gmail.com>
2024-10-23 14:45:57 -0500

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -49,7 +49,7 @@ let options = {
 
     /* Kitiguars Masterlist
     /* --------------------------------------------------------------- */
-    Masterlist: {
+    masterlist: {
 
         sheetPage: sheetPages.kitiguarsMasterlist,
         logSheetPage: sheetPages.kitiguarsMasterlistLog,
@@ -65,7 +65,7 @@ let options = {
 	
     /* Nova's Characters
     /* --------------------------------------------------------------- */
-    Masterlist: {
+    masterlist: {
 
         sheetPage: sheetPages.novasCharacters,
         logSheetPage: sheetPages.novasCharactersLog,
